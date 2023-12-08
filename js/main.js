0e96e7865ebca975703ba5e6cd0b6272785a3acf
shammisharma <hello.shammisharma@gmail.com>
2023-12-08 08:00:07 +0530

    services sec

--- js/main.js
+++ js/main.js
@@ -107,23 +107,22 @@ function hideScroll() {
  
 
 
-new Glide('#glide4', {
-  type: 'carousel',
-  startAt: 0,
-  autoplay: 2000,
-  hoverpause: true,
-  gap: 20,
-  // animationTimingFunc: ease,
-  perView: 5,
-  breakpoints: {
-    800: {
-      perView: 4
-    },
-  600: {
-      perView: 2
-    }
-  }
-}).mount();
+// new Glide('#glide4', {
+//   type: 'carousel',
+//   startAt: 0,
+//   autoplay: 2000,
+//   hoverpause: true,
+//   gap: 20,
+//   perView: 5,
+//   breakpoints: {
+//     800: {
+//       perView: 4
+//     },
+//   600: {
+//       perView: 2
+//     }
+//   }
+// }).mount();
 
 
 new Glide('#testimonials', {
